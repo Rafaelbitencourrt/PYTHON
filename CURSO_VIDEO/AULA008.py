adba5808dbb9805c69af236a5edd12321ae0cac5
Rafael Bitencourt <rafaelbitencourrt@gmail.com>
2023-05-23 13:52:02 -0300

    Add da aula 8 sobre importar módulos

---
+++ CURSO_VIDEO/AULA008.py
@@ -0,0 +1,10 @@
+import math
+
+
+num = int(input('Digte um numero: '))
+raiz = math.sqrt(num)
+
+print('A raiz do numero {} é igual a {}'.format(num, math.ceil(raiz)))
+
+
+##Aula sobre importar modulos.
